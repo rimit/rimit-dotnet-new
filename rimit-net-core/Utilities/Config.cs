825f8b416efc60b2922b9a640a27e2a51d784c79
Rimit <106008521+rimit@users.noreply.github.com>
2024-02-23 12:25:08 +0530

    Update Config.cs

--- rimit-net-core/Utilities/Config.cs
+++ rimit-net-core/Utilities/Config.cs
@@ -7,10 +7,11 @@ namespace RimitNetCore.Utilities
 {
     public class Config
     {
-        // FOR MORE INFO CHECK THE DOCUMENT - https://doc.rimit.co/getting-started/readme#rest
-        public const string BASE_URL = "https://uat-gateway.rimit.in/api/rimit/v1";
+        // FOR MORE INFO CHECK THE DOCUMENT - https://developer.rimit.in/getting-started/readme
+        public const string BASE_URL = "https://uat-gateway.rimit.in/api/client/rimit/v1";  // FOR UAT API
+        // public const string BASE_URL: "https://api-gateway.rimit.in/api/client/rimit/v1", // FOR PRODUCTION API
 
-        // FOR MORE INFO CHECK THE DOCUMENT - https://doc.rimit.co/getting-started/readme#multi-tenant
+        // FOR MORE INFO CHECK THE DOCUMENT - https://developer.rimit.in/getting-started/readme#multi-tenant
         public const string IS_MULTY_TENANT_PLATFORM = "NO"; // OPTIONS - YES/NO
         public const string MULTY_TENANT_MODE = "QUERY"; // OPTIONS - QUERY/PARAMS
     }
